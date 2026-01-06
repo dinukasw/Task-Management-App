d26ed6c1c093bea6b1c276b8f61bdbec1924e194
dinukasw <dinukasanthush50@gmail.com>
2026-01-06 14:00:09 +0530

    chore(db): initialize prisma schema and configure database connection

--- src/validators/task.schema.ts
+++ src/validators/task.schema.ts
@@ -3,7 +3,7 @@ import * as z from "zod";
 export const taskSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters"),
   description: z.string().optional(),
-  status: z.enum(["PENDING", "COMPLETED"]),
+  status: z.enum(["PENDING", "COMPLETED", "CANCELED"]),
 });
 
 export type TaskInput = z.infer<typeof taskSchema>;
